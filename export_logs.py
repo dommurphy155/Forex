452c0a359bbd0fd8e84994fe8875c193b1a44d98
dommurphy155 <dommurphy155@outlook.com>
2025-07-21 22:49:16 +0100

    Update export_logs.py

--- export_logs.py
+++ export_logs.py
@@ -1,5 +1,6 @@
 import logging
 from logging.handlers import RotatingFileHandler
+
 logger = logging.getLogger()
 handler = RotatingFileHandler('forex.log', maxBytes=10_000_000, backupCount=5)
 fmt = '{"time":"%(asctime)s","lvl":"%(levelname)s","msg":%(message)s}'
